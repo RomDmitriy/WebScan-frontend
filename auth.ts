aff0d137822de82c28641b6f993ec72100d56e1b
a.ivashkin <aisek72@gmail.com>
2024-06-03 12:46:09 +0300

    add trust host

--- auth.ts
+++ auth.ts
@@ -1,13 +1,9 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import prisma from './prisma/db';
 
-// const prisma = new PrismaClient();
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
-	//   adapter: PrismaAdapter(prisma),
+	trustHost: true,
 	providers: [
 		GitHub({
 			profile(profile) {
@@ -43,7 +39,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 
 			return true;
 		},
-		async jwt({ token, trigger, profile, account, user }) {
+		async jwt({ token, profile, account }) {
 			if (account && profile) {
 				token.id = profile.id as string;
 				token.accessToken = account.access_token;
